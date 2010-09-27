6bb74ffc494a93e27b9551b2a0e1c2bc16c3df49
dangermaus <dangermaus@users.sourceforge.net>
2010-09-27 19:29:24 +0200

    more tests :-)

--- src/client/packages/own/coremodules/test/testargretrievers.pas
+++ src/client/packages/own/coremodules/test/testargretrievers.pas
@@ -17,18 +17,19 @@ type
   protected
     procedure SetUp; override;
     procedure TearDown; override;
+
+  private
+    procedure checkArguments(argRetr : TArgRetriever);
   end; 
 
 implementation
 
-procedure TTestArgRetriever.TestBasics;
-var argRetr : TArgRetriever;
+procedure TTestArgRetriever.checkArguments(argRetr : TArgRetriever);
+var
     arg     : TArgStk;
     error   : TStkError;
 begin
   clearError(error);
-  argRetr := TArgRetriever.Create('5.345,true,false,'+QUOTE+'test'+QUOTE+',plugcall');
-
   arg := argRetr.getArgument(error);
   AssertEquals('First argument is float type', STK_ARG_FLOAT, arg.argtype);
   AssertEquals('First argument value', 5.345, arg.argvalue);
@@ -49,39 +50,25 @@ begin
   AssertEquals('Fifth argument is call type', STK_ARG_CALL, arg.argtype);
   AssertEquals('Fifth argument value', 'plugcall', arg.argstring);
 
+  arg := argRetr.getArgument(error);
+  AssertEquals('Sixth argument is empty string', STK_ARG_STRING, arg.argtype);
+  AssertEquals('Sixth argument value', '', arg.argstring);
+end;
 
+procedure TTestArgRetriever.TestBasics;
+var argRetr : TArgRetriever;
+begin
+  argRetr := TArgRetriever.Create('5.345,true,false,'+QUOTE+'test'+QUOTE+',plugcall,'+QUOTE+QUOTE);
+  checkArguments(argRetr);
   argRetr.Free;
 end;
 
 
 procedure TTestArgRetriever.TestSpaces;
 var argRetr : TArgRetriever;
-    arg     : TArgStk;
-    error   : TStkError;
 begin
-
-  argRetr := TArgRetriever.Create('   5.345    ,true, false   ,    '+QUOTE+'test'+QUOTE+'  ,    plugcall   ');
-
-  arg := argRetr.getArgument(error);
-  AssertEquals('First argument is float type', STK_ARG_FLOAT, arg.argtype);
-  AssertEquals('First argument value', 5.345, arg.argvalue);
-
-  arg := argRetr.getArgument(error);
-  AssertEquals('Second argument is boolean type', STK_ARG_BOOLEAN, arg.argtype);
-  AssertEquals('Second argument value', true, (arg.argvalue>0));
-
-  arg := argRetr.getArgument(error);
-  AssertEquals('Third argument is boolean type', STK_ARG_BOOLEAN, arg.argtype);
-  AssertEquals('Third argument value', false, (arg.argvalue>0));
-
-  arg := argRetr.getArgument(error);
-  AssertEquals('Fourth argument is string type', STK_ARG_STRING, arg.argtype);
-  AssertEquals('Fourth argument value', 'test', arg.argstring);
-
-  arg := argRetr.getArgument(error);
-  AssertEquals('Fifth argument is call type', STK_ARG_CALL, arg.argtype);
-  AssertEquals('Fifth argument value', 'plugcall', arg.argstring);
-
+  argRetr := TArgRetriever.Create('   5.345    ,true, false   ,    '+QUOTE+'test'+QUOTE+'  ,    plugcall   ,  '+QUOTE+QUOTE+'   ,');
+  checkArguments(argRetr);
   argRetr.Free;
 end;
 
