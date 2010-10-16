a46076458adf28fd204bcd60559b4210bb7753e4
unknown <Danger Mouse@.(none)>
2010-10-16 10:58:28 +0200

    improved schema for database core

--- bin/data/core/sqlite/core-schema.sql
+++ bin/data/core/sqlite/core-schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE tbchat (
    msg text,
    user varchar(32),
    create_dt text,
+   usertime_dt text,
    channel varchar(32)
 );
 
@@ -10,6 +11,8 @@ CREATE TABLE tbjobqueue (
    id int primary key,
    jobid text,
    job text,
+   workunitincoming text,
+   workunitoutgoing text,
    create_dt text
 );
 
@@ -18,32 +21,57 @@ CREATE TABLE tbjobresult (
    jobid text,
    job text,
    result text,
+   workunit_incoming text,
+   workunit_outgoing text,
+   iserroneus numeric,
+   errorid int,
+   errormsg text,
+   errorarg text,
    create_dt text
 );
 
 CREATE TABLE tbnode (
    id int primary key,   
-   node text,
-   speed int,
+   nodeid text,
+   nodename text,
+   country text,
+   region text,
+   ip text,
+   port int
+   localip text,
+   os text,
+   version text,
+   acceptincoming numeric,
+   gigaflops int,
    ram int,
    mhz int,
    nbcpus int,
    isonline numeric,
-   create_dt text
+   uptime real,
+   totaluptime real,
+   cputype text,
+   longitude real,
+   latitude real,
+   create_dt text,
+   update_dt text
 );
 
 CREATE TABLE tbwanparameter (
    id int primary key,
    paramname text,
    paramgroup text,
-   paramvalue text
+   paramvalue text,
+   create_dt text,
+   update_dt text
 );
 
 CREATE TABLE tbparameter (
    id int primary key,
    paramname text,
    paramgroup text,
-   paramvalue text
+   paramvalue text,
+   create_dt text,
+   update_dt text
 );
 
 
