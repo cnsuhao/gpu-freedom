a2f86118ecd917736de4fd15fb4cd51fc11be874
unknown <Danger Mouse@.(none)>
2010-10-16 08:50:27 +0200

    added several tables to core.db

--- bin/data/core/sqlite/core-schema.sql
+++ bin/data/core/sqlite/core-schema.sql
@@ -2,6 +2,48 @@ CREATE TABLE tbchat (
    id int primary key,
    msg text,
    user varchar(32),
-   time text,
+   create_dt text,
    channel varchar(32)
 );
+
+CREATE TABLE tbjobqueue (
+   id int primary key,
+   jobid text,
+   job text,
+   create_dt text
+);
+
+CREATE TABLE tbjobresult (
+   id int primary key,
+   jobid text,
+   job text,
+   result text,
+   create_dt text
+);
+
+CREATE TABLE tbnode (
+   id int primary key,   
+   node text,
+   speed int,
+   ram int,
+   mhz int,
+   nbcpus int,
+   isonline numeric,
+   create_dt text
+);
+
+CREATE TABLE tbwanparameter (
+   id int primary key,
+   paramname text,
+   paramgroup text,
+   paramvalue text
+);
+
+CREATE TABLE tbparameter (
+   id int primary key,
+   paramname text,
+   paramgroup text,
+   paramvalue text
+);
+
+
