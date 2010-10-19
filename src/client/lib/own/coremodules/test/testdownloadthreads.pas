fe78b5f5ab0f65769f8151e1195d838cc77723b3
unknown <Danger Mouse@.(none)>
2010-10-19 21:32:24 +0200

    modified www.google.ch directory

--- src/client/lib/own/coremodules/test/testdownloadthreads.pas
+++ src/client/lib/own/coremodules/test/testdownloadthreads.pas
@@ -26,13 +26,13 @@ begin
   path            := ExtractFilePath(ParamStr(0));
   logger_         := TLogger.Create(path+PathDelim+'logs', 'core.log');
   logger_.setLogLevel(LVL_DEBUG);
-  if FileExists(path+'index.php') then DeleteFile(path+'index.php');
+  if FileExists(path+'index.html') then DeleteFile(path+'index.html');
 
-  downThread_ := TDownloadThread.Create('http://www.gpu-grid.net', path, 'index.php', logger_);
+  downThread_ := TDownloadThread.Create('http://www.google.ch/index.html', path, 'index.html', logger_);
 
   while not downThread_.isDone() do Sleep(100);
 
-  AssertEquals('File index.php exists', true, FileExists(path+'index.php'));
+  AssertEquals('File index.html exists', true, FileExists(path+'index.html'));
   downThread_.Free;
   logger_.Free;
 end;
