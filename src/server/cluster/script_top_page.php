f4b225cca8161d66d8e7ba969cc4a3c21b310d80
dangermaus <dangermaus@users.sourceforge.net>
2010-12-20 12:08:54 +0100

    removed navigation scripts

--- src/server/cluster/script_top_page.php
+++
@@ -1,8 +0,0 @@
-<?php
- include("utils/constants.inc.php");
- include("utils/utils.inc.php");
- 
- $entryoffset = 0;
- js_cookie("entryoffset", $entryoffset);
- js_redirect("list_computers.php");
-?>
