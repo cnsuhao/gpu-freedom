689c28ed9b7497ccebf619f0bb000c778e2dddfa
dangermaus <dangermaus@users.sourceforge.net>
2011-01-25 14:03:37 +0100

    changed http_upload_test.php to be more verbouse

--- src/server/test/http_upload_test.php
+++ src/server/test/http_upload_test.php
@@ -5,15 +5,23 @@
   Source code is under GPL, (c) 2002-2011 the Global Processing Unit Team
   
 */
+ echo "PHP script started\n";
  include("../conf/config.inc.php");
  $type = $_FILES['myfile']['type'];
+ echo "Type is $type.\n";
+
  if ($type == "application/octet-string") {
     $tmp_name = $_FILES['myfile']['tmp_name'];
     $size = $_FILES['myfile']['size']; 
 	$filename = basename($_FILES['myfile']['name']);
+    echo "tmp_name is $tmp_name.\n";
+	echo "size is $size.\n";
+	echo "filename is $filename.\n";
 	
-	//TODO: check if $rootwu configuration path is needed
-	$target_path = "$rootwu/$filename";
-	copy($tmp_name, $target_path);	
+	$target_path = "$filename";
+	copy($tmp_name, $target_path);
+  	echo "File uploaded succesfully\n";
  }
+  echo "PHP script over.\n";
+
 ?>
