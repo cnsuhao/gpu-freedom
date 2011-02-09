99e65c1deb557cb29a8f6b283e3d2a5932b86808
dangermaus <dangermaus@sourceforge.net>
2011-02-09 18:38:44 +0100

    added job reporting serverside, defining maximum of jobs we would like to recieve client side.

---
+++ src/server/jobqueue/report_job.php
@@ -0,0 +1,44 @@
+<?php
+/*
+  This PHP script stores a job from a client into TBJOB and TBJOBQUEUE
+  
+  Source code is under GPL, (c) 2002-2011 the Global Processing Unit Team
+  
+*/
+include("../conf/config.inc.php");
+$nodeid   = $_GET['nodeid'];
+$nodename = $_GET['nodename'];
+$jobid    = $_GET['jobid'];
+$job      = $_GET['job'];
+$workunitincoming = $_GET['workunitincoming'];
+$workunitoutgoing = $_GET['workunitoutgoing'];
+$requests = $_GET['requests'];
+$ip       = $_SERVER['REMOTE_ADDR'];
+
+if (($nodeid=="") || ($jobid=="") || ($job=="") || ($workunitincoming=="") || ($workunitoutgoing=="") || ($requests=="")) die('<b>Parameters not defined</b>');
+if (($requests<1) || ($requests>1000)) die('<b>Too many requests</b>');
+
+mysql_connect($dbserver, $username, $password);
+@mysql_select_db($database) or die("<b>Error: Unable to select database, please check settings in conf/config.inc.php</b>");
+
+$mainquery  = "INSERT INTO tbjob (id, jobid, job, workunitincoming, workunitoutgoing, requests, nodename, nodeid, ip, create_dt) 
+               VALUES('', '$jobid', '$job', '$workunitincoming', '$workunitoutgoing', $requests, '$nodename', '$nodeid', '$ip', NOW());"; 
+$result=mysql_query($mainquery);
+
+// retrievieng the jobid
+$selquery  = "SELECT id FROM tbjob WHERE jobid='$jobid' AND nodeid='$nodeid' AND job='$job';"; 
+$job_id    =  mysql_result($selresult,0,"id");
+
+$i=0;
+while ($i<$requests) {
+
+    $jqquery  = "INSERT INTO tbjobqueue (id, job_id, nodeid, create_dt) 
+                 VALUES('', $job_id, '$nodeid', NOW());"; 
+    $jqresult=mysql_query($jqquery);
+
+	$i++;
+}
+
+mysql_close();
+
+?>
