ec20e42afd0df37a990002b000e48793839a08f7
dangermaus <dangermaus@sourceforge.net>
2011-02-09 19:21:42 +0100

    http upload test less verbouse

--- src/server/test/http_upload_test.php
+++ src/server/test/http_upload_test.php
@@ -5,23 +5,28 @@
   Source code is under GPL, (c) 2002-2011 the Global Processing Unit Team
   
 */
- echo "PHP script started\n";
+ // echo "PHP script started\n";
  include("../conf/config.inc.php");
  $type = $_FILES['myfile']['type'];
- echo "Type is $type.\n";
+ // echo "Type is $type.\n";
 
  if ($type == "application/octet-string") {
     $tmp_name = $_FILES['myfile']['tmp_name'];
     $size = $_FILES['myfile']['size']; 
 	$filename = basename($_FILES['myfile']['name']);
-    echo "tmp_name is $tmp_name.\n";
+    /*
+	echo "tmp_name is $tmp_name.\n";
 	echo "size is $size.\n";
 	echo "filename is $filename.\n";
+	*/
 	
 	$target_path = "$filename";
 	copy($tmp_name, $target_path);
-  	echo "File uploaded succesfully\n";
+  	//echo "File uploaded succesfully\n";
+	echo "OK\n";
+ } else {
+	echo "FAIL\n"; 
  }
-  echo "PHP script over.\n";
+ //echo "PHP script over.\n";
 
 ?>
