607210aa2b7c914474905bc95cad795366d48085
dangermaus <dangermaus@sourceforge.net>
2011-02-13 21:25:05 +0100

    added script to prepare package for release

---
+++ install/dev/cleancode.sh
@@ -0,0 +1,16 @@
+rm ../../bin/*.log
+rm ../../bin/*.ppu
+rm ../../bin/*.php
+rm ../../bin/*.or
+rm ../../bin/*.res
+rm ../../bin/*.o
+rm ../../bin/*.compiled
+rm ../../bin/online.*
+rm ../../bin/logs/*.log
+rm ../../bin/core.db
+rm ../../bin/plugins/lib/*.ppu
+rm ../../bin/plugins/lib/*.o
+rm ../../bin/plugins/lib/*.RES
+rm ../../bin/plugins/lib/*.res
+rm ../../bin/plugins/lib/*.compiled
+rm ../../bin/plugins/lib/*.or
