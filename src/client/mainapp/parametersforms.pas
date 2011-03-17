28992ce0f9ccb5e541838806395f9af3031b2797
dangermaus <dangermaus@users.sourceforge.net>
2011-03-17 19:53:29 +0100

    adding all fields to the form

--- src/client/mainapp/parametersforms.pas
+++ src/client/mainapp/parametersforms.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, ExtCtrls;
+  StdCtrls, ExtCtrls, MaskEdit, Spin;
 
 type
 
@@ -15,6 +15,15 @@ type
   TParametersForm = class(TForm)
     btnSave: TButton;
     btnReset: TButton;
+    cbLatDir: TComboBox;
+    cbTeam: TComboBox;
+    cbLonDir: TComboBox;
+    cbRunOnlyIdle: TCheckBox;
+    edtLonSeconds: TEdit;
+    edtLonMinutes: TEdit;
+    edtCity: TEdit;
+    edtLatMinutes: TEdit;
+    edtLatSeconds: TEdit;
     gbNode: TGroupBox;
     gbConnection: TGroupBox;
     gbUser: TGroupBox;
@@ -22,6 +31,27 @@ type
     edtNodename: TLabeledEdit;
     edtProxyHost: TLabeledEdit;
     edtProxyPort: TLabeledEdit;
+    edtRegion: TLabeledEdit;
+    edtStreet: TLabeledEdit;
+    edtZip: TLabeledEdit;
+    edtDescription: TLabeledEdit;
+    edtLonDegrees: TLabeledEdit;
+    edtLatDegrees: TLabeledEdit;
+    edtUsername: TLabeledEdit;
+    edtEmail: TLabeledEdit;
+    edtRealname: TLabeledEdit;
+    edtHomepage: TLabeledEdit;
+    lblMaxUploads: TLabel;
+    lblMaxDownloads: TLabel;
+    lblMaxServices: TLabel;
+    lblMaxComputations: TLabel;
+    lblPassword: TLabel;
+    lblTeam: TLabel;
+    edtPassword: TMaskEdit;
+    edtMaxComputations: TSpinEdit;
+    edtMaxServices: TSpinEdit;
+    edtMaxDownloads: TSpinEdit;
+    edtMaxUploads: TSpinEdit;
   private
     { private declarations }
   public
