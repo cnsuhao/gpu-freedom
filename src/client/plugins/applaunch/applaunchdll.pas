f6faab5974dff81a88597fd25d981d99a7114551
dangermaus <dangermaus@users.sourceforge.net>
2011-08-26 17:38:49 +0200

    adding basic functionality to applaunch

--- src/client/plugins/applaunch/applaunchdll.pas
+++ src/client/plugins/applaunch/applaunchdll.pas
@@ -4,7 +4,7 @@ unit applaunchdll;
 
 interface
 
-uses SysUtils, stacks, formatsets;
+uses SysUtils, stacks, formatsets, Process;
 
 function description    : TStkString;
 function weburl         : TStkString;
@@ -31,12 +31,37 @@ begin
   Result := STACK_VERSION;
 end;
 
+function launch(var stk: TStack; wait : Boolean) : boolean;
+var a,b : TStkString;
+    aProcess : TProcess;
+begin
+ Result := retrieveStringParams(a, b, stk);
+ if not Result then Exit;
+
+ Result := false;
+ AProcess := TProcess.Create(nil);
+ try
+    AProcess.CommandLine := '"'+a+'" "'+b+'"';
+    if wait then
+         AProcess.Options := AProcess.Options + [poWaitOnExit]
+    else
+         AProcess.Options := AProcess.Options - [poWaitOnExit];
+
+    AProcess.Execute;
+    Result := true;
+ finally
+    AProcess.Free;
+ end;
+end;
+
 function launch_wait(var stk: TStack): boolean;
 begin
+  Result := launch(stk, true);
 end;
 
 function launch_nowait(var stk: TStack): boolean;
 begin
+  Result := launch(stk, false);
 end;
 
 
