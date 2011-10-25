f1a64a315cfce4d7275210f82af98933f30383bd
dangermaus <dangermaus@users.sourceforge.net>
2011-10-25 08:42:24 +0200

    added function  extractparamlong with ansistring

--- src/client/lib/own/misc/utils.pas
+++ src/client/lib/own/misc/utils.pas
@@ -5,10 +5,29 @@ interface
 uses SysUtils;
 
 function ExtractParam(var S: string; Separator: string): string;
+function ExtractParamLong(var S: AnsiString; Separator: string): AnsiString;
 
 implementation
 
 function ExtractParam(var S: string; Separator: string): string;
+var
+  i: Longint;
+begin
+  i := Pos(Separator, S);
+  if i > 0 then
+  begin
+    Result := Copy(S, 1, i - 1);
+    Delete(S, 1, i);
+  end
+  else
+  begin
+    Result := S;
+    S      := '';
+  end;
+end;
+
+
+function ExtractParamLong(var S: AnsiString; Separator: string): AnsiString;
 var
   i: Longint;
 begin
