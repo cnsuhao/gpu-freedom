873e618d7143b071134a5b570e83b3740e04f56d
dangermouse <tiziano.mengotti@gmail.com>
2012-02-22 17:41:05 +0100

    added quicksort implementation

--- src/client/lib/own/misc/utils.pas
+++ src/client/lib/own/misc/utils.pas
@@ -6,6 +6,7 @@ uses SysUtils;
 
 function ExtractParam(var S: string; Separator: string): string;
 function ExtractParamLong(var S: AnsiString; Separator: string): AnsiString;
+procedure QuickSort(var A: array of Integer; iLo, iHi: Integer) ;
 
 implementation
 
@@ -43,5 +44,31 @@ begin
     S      := '';
   end;
 end;
+
+// from http://delphi.about.com/od/objectpascalide/a/quicksort.htm
+// Implementing QuickSort Sorting Algorithm in Delphi
+// By Zarko Gajic
+procedure QuickSort(var A: array of Integer; iLo, iHi: Integer) ;
+ var
+   Lo, Hi, Pivot, T: Integer;
+ begin
+   Lo := iLo;
+   Hi := iHi;
+   Pivot := A[(Lo + Hi) div 2];
+   repeat
+     while A[Lo] < Pivot do Inc(Lo) ;
+     while A[Hi] > Pivot do Dec(Hi) ;
+     if Lo <= Hi then
+     begin
+       T := A[Lo];
+       A[Lo] := A[Hi];
+       A[Hi] := T;
+       Inc(Lo) ;
+       Dec(Hi) ;
+     end;
+   until Lo > Hi;
+   if Hi > iLo then QuickSort(A, iLo, Hi) ;
+   if Lo < iHi then QuickSort(A, Lo, iHi) ;
+ end;
 
 end.
