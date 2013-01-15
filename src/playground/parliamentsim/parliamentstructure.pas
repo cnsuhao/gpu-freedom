62939742cf65e8d25ec84fb08440c4882dee8620
dangermouse <dangermaus@users.sf.net>
2013-01-15 15:19:42 +0100

    in the middle of filling the structure

--- src/playground/parliamentsim/parliamentstructure.pas
+++ src/playground/parliamentsim/parliamentstructure.pas
@@ -9,10 +9,14 @@ uses
 
 const MAX_DELEGATES = 300;
       MAX_PARTIES = 5;
+      INDIPENDENT = -1;
 
 type TDelegate = record
     personalinterest,
     collectiveinterest : Extended; // values between -1..1
+
+    party : Longint; // index to the party to which the delegate belongs
+                     // -1 if INDIPENDENT
 end;
 
 type TDelegates = record
@@ -21,7 +25,6 @@ type TDelegates = record
 end;
 
 type TParty  = record
-    members : Array[1..MAX_DELEGATES] of Longint; // index pointing to delegates
     size    : Longint;
 
     centerx,
@@ -42,20 +45,54 @@ end;
 var rndgen     : TIsaac;
     parliament : TParliament;
 
+procedure logText(str : AnsiString);
 function getRndminusOnetoOne : Extended;
 procedure initParliament(nbdelegates, nbindipendents, nbparties : Longint; partyradius : Extended);
+procedure printParliament;
 
 implementation
+
+
+procedure logText(str : AnsiString);
+begin
+
+end;
+
+procedure printParliament;
+begin
+
+end;
 
 function getRndminusOnetoOne : Extended;
 begin
   Result := ( ( rndgen.Val/High(cardinal) )  * 2) - 1;
 end;
 
+// this garuantess a center with a complete circle
+function getPartyCenter(partyradius : Extended) : Extended;
+begin
+  Result := ( ( rndgen.Val/High(cardinal) )  * (2-2*partyradius)  ) - 1 + partyradius;
+end;
+
 procedure initDelegate(i : Longint);
 begin
   parliament.delegates.delg[i].personalinterest := getRndminusOnetoOne;
   parliament.delegates.delg[i].collectiveinterest := getRndminusOnetoOne;
+
+  parliament.delegates.delg[i].party := INDIPENDENT;
+end;
+
+procedure initParty(i : Longint; partyradius : Extended);
+begin
+  parliament.parties.par[i].centerx:= getPartyCenter(partyradius);
+  parliament.parties.par[i].centery:= getPartyCenter(partyradius);
+
+  parliament.parties.par[i].size := 0;
+end;
+
+procedure assignDelegatesWithPartyRadius(party : Longint);
+begin
+
 end;
 
 procedure initParliament(nbdelegates, nbindipendents, nbparties : Longint; partyradius : Extended);
@@ -64,11 +101,26 @@ begin
  if nbdelegates>MAX_DELEGATES then raise Exception.Create('Too many delegetes!');
 
  parliament.delegates.size:=nbdelegates;
+ parliament.parties.size := nbparties;
+
  for i:=1 to nbdelegates do
      begin
         initDelegate(i);
      end;
 
+ // we define the parties and their radius
+ for i:=1 to parliament.parties.size do
+     begin
+        initParty(i, partyradius);
+     end;
+
+ // we have two rounds to assign delegates to parties, first we use the setting partyradius
+ for i:=1 to parliament.parties.size do
+     begin
+        assignDelegatesWithPartyRadius(i);
+     end;
+
+ // to reach the proposed quota of indipendents, we take delegates and assign them to the party
 end;
 
 initialization
