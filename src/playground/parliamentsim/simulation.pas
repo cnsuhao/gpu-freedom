34c7fc30f716540d21de62f7a603dc4a56c8ad29
dangermouse <dangermaus@users.sf.net>
2013-01-16 17:24:04 +0100

    adding laws and initial part of simulation

--- src/playground/parliamentsim/simulation.pas
+++ src/playground/parliamentsim/simulation.pas
@@ -22,13 +22,16 @@ begin
   proposer := selectRandomDelegate(parliament);
   laws.laws[l].personalinterestx:=parliament.delegates.delg[proposer].personalinterestx;
   laws.laws[l].collectiveinteresty:=parliament.delegates.delg[proposer].collectiveinteresty;
+
+  laws.laws[l].yes := 0;
+  laws.laws[l].no  := 0;
 end;
 
 
 procedure simulateLawProcess(var parliament : TParliament; var laws : TLaws; l : Longint);
 begin
    createLaw(parliament, laws, l);
-
+   voteLaw(parliament, laws, l);
 end;
 
 procedure simulateParliament(var parliament : TParliament; var laws : TLaws);
