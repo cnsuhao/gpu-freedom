bea75f9d03e34902674ee4e8d7a8e54633e56f34
dangermouse <dangermaus@users.sf.net>
2013-02-01 14:58:24 +0100

    plotting cube

--- src/playground/cube/freepascal/cube.lpr
+++ src/playground/cube/freepascal/cube.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, lazopenglcontext, cubeUnit
+  Forms, lazopenglcontext, cubeUnit, plotcubes
   { you can add units after this };
 
 {$R *.res}
---
+++ src/playground/cube/freepascal/plotcubes.pas
@@ -0,0 +1,27 @@
+unit plotcubes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+procedure initCubePlot;
+procedure plotCube;
+
+implementation
+
+procedure initCubePlot;
+begin
+
+end;
+
+procedure plotCube;
+begin
+
+end;
+
+
+end.
+
