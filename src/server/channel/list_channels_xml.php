99b8c16c46052e2535baa0cbc29e888423b62470
dangermouse <dangermaus@users.sf.net>
2013-02-19 16:20:28 +0100

    missing semicolon

--- src/server/channel/list_channels_xml.php
+++ src/server/channel/list_channels_xml.php
@@ -5,7 +5,8 @@
   Source code is under GPL, (c) 2002-2013 the Global Processing Unit Team
   
 */
-include('../utils/sql2xml.php') 
+
+include('../utils/sql2xml.php'); 
  
 echo "<channeltype>\n";
 
@@ -13,4 +14,5 @@ $level_list = Array("type");
 sql2xml('select distinct c.channame, c.chantype from tbchannel c', $level_list, 0);
 
 echo "</channeltype>\n";
+
 ?>
