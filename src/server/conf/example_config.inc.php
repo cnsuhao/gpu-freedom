1b1dc8529000379a73f27d2fbdea53c5b857a183
dangermouse <dangermaus@users.sf.net>
2013-02-22 14:12:50 +0100

    added settings to configuration file which represent our server

--- src/server/conf/example_config.inc.php
+++ src/server/conf/example_config.inc.php
@@ -3,9 +3,19 @@
  User settings are defined here
 */
 
-// mySQL settings
-$username="gpuuser";
-$password="";
-$database="gpu_server";
-$dbserver="localhost";
+// mySQL settings to connect to db schema
+$username = "gpuuser";
+$password = "gpupwd";
+$database = "gpu_server";
+$dbserver = "localhost";
+
+
+// GPU server settings, transmitted to other superservers
+$my_server_name = "Altos";
+$my_server_url  = "http://127.0.0.1:8090/gpu_freedom/src/server";
+$my_default_chat_channel = "altos";
+$am_i_superserver = false; // specifies if this server collects information from other servers
+$my_longitude = 14;
+$my_latitude  = 10;
+
 ?>
