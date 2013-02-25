21e31a203bf4e0dbe276b604b3b90048b8fd2baf
dangermouse <dangermaus@users.sf.net>
2013-02-25 11:27:57 +0100

    defining some server wide parameters

---
+++ src/server/install/install_execution.php
@@ -0,0 +1,31 @@
+<?php
+	
+
+
+ include("../conf/config.inc.php");	
+ // defines parameters in the database
+ include("../utils/parameters.inc.php");
+ 
+  mysql_connect($dbserver, $username, $password);
+ @mysql_select_db($database) or die("ERROR: Unable to select database, please check settings in conf/config.inc.php'\n");
+ 
+ // define an unique server id for this installation
+ if (get_db_parameter("CONFIGURATION", "SERVER_ID", "missing")=="missing") {
+	$c        = uniqid (rand (),true);
+    $serverid = md5($c);
+    set_db_parameter("CONFIGURATION", "SERVER_ID", $serverid);
+ }
+ 
+ if (get_db_parameter("TIME", "UPTIME", "missing")=="missing") {
+	set_db_parameter("TIME", "UPTIME", "0");
+ }
+
+ if (get_db_parameter("TIME", "TOTAL_UPTIME", "missing")=="missing") {
+	set_db_parameter("TIME",  "TOTAL_UPTIME", "0");
+ }
+
+ 
+ mysql_close();
+ 
+ echo "OK";
+?>
