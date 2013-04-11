44d99e1635bdedf766076e1ca90b2057c393eacc
dangermouse <dangermaus@users.sf.net>
2013-04-11 16:16:28 +0200

    form to submit job is finished

--- src/client/mainapp/jobmanagementforms.pas
+++ src/client/mainapp/jobmanagementforms.pas
@@ -6,25 +6,36 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  ExtCtrls, ComCtrls, StdCtrls, jobapis;
+  ExtCtrls, ComCtrls, StdCtrls, Spin, jobapis;
 
 type
 
   { TJobManagementForm }
 
   TJobManagementForm = class(TForm)
+    btnSelectInputWorkunit: TButton;
+    btnSubmitJob: TButton;
     cbJobBox: TComboBox;
     cbJobType: TComboBox;
     cbRequireAck: TCheckBox;
+    cbTagOutputWorkunit: TCheckBox;
     cgWorkflow: TCheckGroup;
+    cbTagInputWorkunit: TCheckBox;
+    edtWorkunitInput: TEdit;
+    edtWorkunitOutput: TEdit;
     gbWorkunits: TGroupBox;
+    lblNumberOfRequests: TLabel;
+    lblOutputWorkunitName: TLabel;
+    lblWorkunitInput: TLabel;
     lblJobType: TLabel;
     lblJob: TLabel;
     lblJobDefinitionIdDesc: TLabel;
     lblJobDefinitionId: TLabel;
+    OpenDialog: TOpenDialog;
     pnCreateJob: TPanel;
     rbGlobal: TRadioButton;
     rbLocal: TRadioButton;
+    seNbRequests: TSpinEdit;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure lblJobDefinitionIdDescClick(Sender: TObject);
