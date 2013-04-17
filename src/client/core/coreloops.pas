bd5e854ebdebd7aab7bb5386d304633cd8073beb
dangermouse <dangermaus@users.sf.net>
2013-04-17 12:04:34 +0200

    disabling retrieval of jobs to investigate server workflow

--- src/client/core/coreloops.pas
+++ src/client/core/coreloops.pas
@@ -150,7 +150,8 @@ begin
       if lf_morefrequentupdates.exists  and (tick_ mod 13 = 0) then retrieveChannels;
 
       receiveJobs := tick_ mod myConfId.receive_jobs_each;
-      if (receiveJobs = 0)  then retrieveJobs;
+      //TODO: enable this:
+      //if (receiveJobs = 0)  then retrieveJobs;
       if (receiveJobs = 29) then retrieveJobStats;
 
       // ***************************************************
