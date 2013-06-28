782ebcde825b364b8fe248a66d660062dbcf0574
dangermouse <dangermaus@users.sf.net>
2013-06-28 15:08:34 +0200

    parsing of frequency values over

--- src/playground/powergrid/frequencycollector_swissgrid.sh
+++ src/playground/powergrid/frequencycollector_swissgrid.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# main page is http://www.swissgrid.ch/content/swissgrid/de/home/experts/topics/frequency.html 
 rm -rf frequency.html
-wget -O frequency.html --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" http://www.swissgrid.ch/content/swissgrid/de/home/experts/topics/frequency.html 
+wget -O frequency.html --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" http://www.swissgrid.ch/swissgrid/de/applicationFrequencyTimeAjax
 ./removeif0.sh frequency.html
 if [ -f frequency.html ] 
 then
-    wget -O store_frequency_swissgrid.html --no-proxy http://127.0.0.1:8090/bitcoin/store_frequency_swissgrid.php
+    wget -O store_frequency_swissgrid.html --no-proxy http://127.0.0.1:8090/gpu_freedom/src/playground/powergrid/store_frequency_swissgrid.php
 fi
