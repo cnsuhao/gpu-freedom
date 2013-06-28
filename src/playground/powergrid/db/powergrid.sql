a0f709c0c3cd8392e71855126b421ba16052b720
dangermouse <dangermaus@users.sf.net>
2013-06-28 15:25:56 +0200

    db with first data

--- src/playground/powergrid/db/powergrid.sql
+++ src/playground/powergrid/db/powergrid.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 28, 2013 at 11:35 AM
+-- Generation Time: Jun 28, 2013 at 03:25 PM
 -- Server version: 5.5.25a
 -- PHP Version: 5.4.4
 
@@ -35,7 +35,16 @@ CREATE TABLE IF NOT EXISTS `frequency` (
   `create_dt` datetime NOT NULL,
   `create_user` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `frequency`
+--
+
+INSERT INTO `frequency` (`id`, `frequency`, `networkdiff`, `controlarea`, `tso`, `create_dt`, `create_user`) VALUES
+(2, 50.007, 13.201, 'SG_ST', 'Swissgrid', '2013-06-28 15:23:06', 'php'),
+(3, 50.013, 13.14, 'SG_ST', 'Swissgrid', '2013-06-28 15:23:56', 'php'),
+(4, 50.01, 13.149, 'SG_ST', 'Swissgrid', '2013-06-28 15:24:22', 'php');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
