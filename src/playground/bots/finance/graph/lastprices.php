a8ae615d0c0f0a98f40199c5f1d8b449b04af1c0
dangermouse <dangermaus@users.sf.net>
2013-07-09 16:22:12 +0200

    lastprices works

---
+++ src/playground/bots/finance/graph/lastprices.php
@@ -0,0 +1,54 @@
+<?php
+include_once( '../utils/openflashchart/open-flash-chart.php' );
+include("../conf/config.inc.php");
+if (isset($_GET['ticker'])) $ticker=$_GET['ticker']; else die("<b>Ticker Parameter not defined</b>");
+
+ // preparing data for chart
+ mysql_connect($dbserver, $username, $password);
+ @mysql_select_db($database) or die("Unable to select database");
+ $query="select value from tickers where name='$ticker' AND (create_dt>NOW() - INTERVAL 30 DAY) order by create_dt asc LIMIT 300;";
+ $result=mysql_query($query);
+ 
+ if ($result=="") {
+    mysql_close();
+    exit();
+ } else {
+  $num=mysql_numrows($result); 
+ }
+ 
+ $data = array();
+ $labels = array();
+ $i=0;
+ $max=100;
+ while ($i<$num) { 
+   $price=mysql_result($result,$i,"value");          
+   
+   $data[$i] = $price;
+   $labels[$i] = '';
+   
+   if ($price>$max) $max=$price;
+   
+   $i++;
+ }
+ mysql_close();
+ 
+
+
+// use the chart class to build the chart:
+$g = new graph();
+$g->title( "Last Prices for $ticker", '{font-size:18px; color: #d01f3c}' );
+
+//
+// pass in two arrays, one of data, the other data labels
+//
+$g->set_data($data);
+$g->set_x_labels($labels);
+$g->set_x_label_style( 10, '#9933CC', 0, 2 );
+$g->set_y_max($max);
+
+$g->set_tool_tip( '#val#' );
+
+
+// display the data
+echo $g->render();
+?>
