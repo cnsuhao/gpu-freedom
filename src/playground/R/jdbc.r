46c0e1256b310333637a9378761de8795fe9337e
dangermouse <dangermaus@users.sf.net>
2013-10-01 09:47:47 +0200

    my first R routines

---
+++ src/playground/R/jdbc.r
@@ -0,0 +1,19 @@
+library(RJDBC);
+source("D:\\xampp\\htdocs\\gpu_freedom\\src\\playground\\R\\config.r")
+drv <- JDBC("com.mysql.jdbc.Driver", "D:\\jdbc\\mysql-connector-java-5.1.5-bin.jar",identifier.quote="`");
+
+b_conn  <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/bitcoin", username, password);
+b_pricetable <- dbReadTable(b_conn, "pricevalue")
+b_price      <- dbGetQuery(b_conn, "select price from pricevalue order by id asc")
+b_lastprice  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt>(NOW() - INTERVAL 7 DAY)  order by id asc")
+
+
+p_conn      <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/powergrid", username, password);
+p_freqtable <- dbReadTable(p_conn, "tbfrequency")
+p_freq      <- dbGetQuery(p_conn, "select frequencyhz from tbfrequency order by id asc")
+p_netdiff   <- dbGetQuery(p_conn, "select networkdiff from tbfrequency order by id asc")
+
+f_conn <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/finance", username, password);
+f_tickerstable <- dbReadTable(f_conn, "tickers")
+f_vix  <- dbGetQuery(f_conn, "select value from tickers where name='VIX' order by id asc")
+f_sp   <- dbGetQuery(f_conn, "select value from tickers where name='GSPC' order by id asc")
---
+++ src/playground/R/tiz6pack.r
@@ -0,0 +1,49 @@
+# tiz6pack, a library with some utils for R
+# (c) by 2013 HB9TVM, source code is under GPL
+
+# This function retrieves a measure of smoothness for a given curve
+# This is similar concept to Variance.
+# Formula is
+# smoothness= Sum_i[ (x_i - ((x_i+1+x_i-1)/2)]/n or n-1 if sample is set to true
+# test with:
+# 
+#
+#  library(ISwR)
+#  snessvar(thuesen$blood.glucose)
+#  sness(thuesen$blood.glucose)
+snessvar <- function(x, sample=FALSE) {
+  N <- length(x)
+  xbar <- 1:N
+  
+  for(i in 1:N) {
+       if (i==1) {
+			xbar[1]=x[1];
+	   } else
+	   if (i==N) {
+	        xbar[N]=x[N];  
+	   } else {
+            xbar[i] = (x[i+1] + x[i-1])/2; 
+	   }
+  }
+  
+  smoothsst = sum((x - xbar)^2)
+  
+  if (sample) {
+	smoothvar <- smoothsst/(N-1)
+  } else {
+    smoothvar <- smoothsst/N
+  }
+  
+  return(smoothvar)
+}
+
+# Square root of Smoothness
+# This is similar to Standard Deviation
+sness <- function(x, sample=FALSE) {
+   return( sqrt( snessvar(x, sample) ) )
+}
+
+# body mass index
+bmi <- function(weight, height) {
+	return(weight/height^2)
+}
