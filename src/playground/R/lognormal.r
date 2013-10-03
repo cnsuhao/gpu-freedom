c8c244f43277a85f4dc22a40e18d832a04bc4dc6
dangermouse <dangermaus@users.sf.net>
2013-10-03 11:58:50 +0200

    fixed lognormal problem. first cool graph

--- src/playground/R/lognormal.r
+++ src/playground/R/lognormal.r
@@ -5,23 +5,29 @@ o<-order(b_price$price)
 b_price$price[o]
 #plot(b_avg_price$usd[o])
 #points(b_avg_price$usd[o]help)
-hist(b_price$price, breaks=12)
+#hist(b_price$price, breaks=12)
 
-x <- b_price$price
-N<-length(x)
-bitcoinm  <- mean(x)
-bitcoinsd <- sd(x)
+x=b_price$price
+N=length(x)
+m= mean(x)
+v= var(x)
 
-# from https://stat.ethz.ch/pipermail/r-help/2008-May/161229.html
-# A mean of 100 for the log-normal variate?  In this case any set of mu
-# and sd for which exp(mu+sd^2/2)=100 (or mu+sd^2/2=log(100)) would do
-# the trick
-logmu = log(bitcoinm)/2
-logsd = sqrt(log(bitcoinm))
+# from http://www.mathworks.ch/ch/help/stats/lognrnd.html
+# Generate one million lognormally distributed random numbers with mean m and sigma s
+mu = log((m^2)/sqrt(v+m^2));
+sigma = sqrt(log(v/(m^2)+1));
 
-cmplognormal <- rlnorm(N, meanlog=logmu, sdlog=logsd)
-# rlnorm seems broken to me!
+# normal and lognormal series
+cmplognormal <- rlnorm(N, meanlog=mu, sdlog=sigma)
 cmpnormal<- rnorm(N, mean=bitcoinm, sd=bitcoinsd)
-plot(cmpnormal)
-hist(cmpnormal, breaks=12)
-print(chisq.test(x, cmpnormal))
+
+# plots
+
+hist(cmpnormal, breaks=12, col = "lightyellow", border = "pink", main="", xlab="", ylab="")
+hist(cmplognormal, breaks=12, col = "lightblue", border = "pink", add=T, main="", xlab="", ylab="")
+hist(x,col= "gray", add=T, main="", xlab="", ylab="")
+title(main="Bitcoin Price Distribution", xlab="USD", ylab="freq")
+
+# chi squared test
+print(chisq.test(x, cmplognormal))
+
