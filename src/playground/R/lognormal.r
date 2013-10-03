d157225ba080606d3e5b98dda50cb995a4293ba3
dangermouse <dangermaus@users.sf.net>
2013-10-03 11:36:49 +0200

    still some problems with the lognormal dist II

--- src/playground/R/lognormal.r
+++ src/playground/R/lognormal.r
@@ -7,13 +7,19 @@ b_price$price[o]
 #points(b_avg_price$usd[o]help)
 hist(b_price$price, breaks=12)
 
-x <- b_price$price/100
-N<-length(b_price$price)
+x <- b_price$price
+N<-length(x)
 bitcoinm  <- mean(x)
 bitcoinsd <- sd(x)
 
-# there seems to be an error with these two parameters and rlnorm
-cmplognormal <- rlnorm(N, meanlog=exp(bitcoinm), sdlog=exp(bitcoinsd))
+# from https://stat.ethz.ch/pipermail/r-help/2008-May/161229.html
+# A mean of 100 for the log-normal variate?  In this case any set of mu
+# and sd for which exp(mu+sd^2/2)=100 (or mu+sd^2/2=log(100)) would do
+# the trick
+logmu = log(bitcoinm)/2
+logsd = sqrt(log(bitcoinm))
+
+cmplognormal <- rlnorm(N, meanlog=logmu, sdlog=logsd)
 # rlnorm seems broken to me!
 cmpnormal<- rnorm(N, mean=bitcoinm, sd=bitcoinsd)
 plot(cmpnormal)
