88070592d379beb94d83d8e0cabe7bb629a05520
dangermouse <dangermaus@users.sf.net>
2013-10-03 11:22:58 +0200

    still some problems with the lognormal dist

--- src/playground/R/lognormal.r
+++ src/playground/R/lognormal.r
@@ -1,20 +1,21 @@
 # the goal here is to show that bitcoin prices are lognormal distributed
 # using plots and a chi squared test
 # (c) 2013 by HB9TVM
-o<-order(b_avg_price$usd)
-b_avg_price$usd[o]
+o<-order(b_price$price)
+b_price$price[o]
 #plot(b_avg_price$usd[o])
 #points(b_avg_price$usd[o]help)
-hist(b_avg_price$usd, breaks=12)
+hist(b_price$price, breaks=12)
 
-N<-length(b_avg_price$usd)
-bitcoinm  <- mean(b_avg_price$usd)
-bitcoinsd <- sd(b_avg_price$usd)
+x <- b_price$price/100
+N<-length(b_price$price)
+bitcoinm  <- mean(x)
+bitcoinsd <- sd(x)
 
 # there seems to be an error with these two parameters and rlnorm
-cmplognormal <- rlnorm(N, meanlog=bitcoinm, sdlog=bitcoinsd)
+cmplognormal <- rlnorm(N, meanlog=exp(bitcoinm), sdlog=exp(bitcoinsd))
 # rlnorm seems broken to me!
 cmpnormal<- rnorm(N, mean=bitcoinm, sd=bitcoinsd)
 plot(cmpnormal)
 hist(cmpnormal, breaks=12)
-print(chisq.test(b_avg_price$usd, cmpnormal))
+print(chisq.test(x, cmpnormal))
