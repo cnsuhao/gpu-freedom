888869ce92c8e2d5921cade405e282e39a0aedda
dangermouse <dangermaus@users.sf.net>
2013-10-03 10:09:13 +0200

    implemented avg of jdbc data set for R analysis

--- src/playground/R/jdbc.r
+++ src/playground/R/jdbc.r
@@ -5,18 +5,23 @@ drv <- JDBC("com.mysql.jdbc.Driver", "D:\\jdbc\\mysql-connector-java-5.1.5-bin.j
 b_conn  <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/bitcoin", username, password);
 b_pricetable <- dbReadTable(b_conn, "pricevalue")
 b_price      <- dbGetQuery(b_conn, "select price from pricevalue order by id asc")
-b_lastprice  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt>(NOW() - INTERVAL 7 DAY)  order by id asc")
-
+b_last_price  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt>(NOW() - INTERVAL 7 DAY)  order by id asc")
+b_avg_price   <- dbGetQuery(b_conn, "SELECT DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG( price ) as usd  FROM pricevalue GROUP BY date ORDER BY date")
 
 p_conn      <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/powergrid", username, password);
 p_freqtable <- dbReadTable(p_conn, "tbfrequency")
 p_freq      <- dbGetQuery(p_conn, "select frequencyhz from tbfrequency order by id asc")
-p_netdiff   <- dbGetQuery(p_conn, "select networkdiff from tbfrequency order by id asc")
+p_avg_freq  <- dbGetQuery(p_conn, "select DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG(frequencyhz) as hz from tbfrequency GROUP BY date ORDER BY date")
+
+p_netdiff     <- dbGetQuery(p_conn, "select networkdiff from tbfrequency order by id asc")
+p_avg_netdiff <- dbGetQuery(p_conn, "select DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG(networkdiff) as s from tbfrequency GROUP BY date ORDER BY date")
 
 f_conn <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/finance", username, password);
 f_tickerstable <- dbReadTable(f_conn, "tickers")
-f_vix  <- dbGetQuery(f_conn, "select value from tickers where name='VIX' order by id asc")
-f_sp   <- dbGetQuery(f_conn, "select value from tickers where name='GSPC' order by id asc")
+f_vix       <- dbGetQuery(f_conn, "select value from tickers where name='VIX' order by id asc")
+f_avg_vix   <- dbGetQuery(f_conn, "select DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG(value) as usd from tickers where name='VIX' GROUP BY date ORDER BY date")
+f_sp        <- dbGetQuery(f_conn, "select value from tickers where name='GSPC' order by id asc")
+f_avg_sp    <- dbGetQuery(f_conn, "select DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG(value) as usd from tickers where name='GSPC' GROUP BY date ORDER BY date")
 
 # analyze(b_price$price)
 # analyze(b_lastprice$price)
