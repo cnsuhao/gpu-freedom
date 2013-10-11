943bf8747d532f255e454f552f7972ac91a9935e
dangermouse <dangermaus@users.sf.net>
2013-10-11 09:20:18 +0200

    added some quantmod basics

--- src/playground/R/quantmod.r
+++ src/playground/R/quantmod.r
@@ -1,2 +1,18 @@
+# from http://www.quantmod.com/examples/intro/
 library(quantmod)
-getSymbols("YHOO",src="google")
+getSymbols("YHOO",src="google") # from google finance
+getSymbols("GOOG",src="yahoo") # from yahoo finance 
+barChart(GOOG)
+
+getSymbols("XPT/USD",src="oanda") 
+chartSeries(XPTUSD,name="Platinum (.oz) in $USD")
+# platinum, weekly with candles
+chartSeries(to.weekly(XPTUSD),up.col='white',dn.col='blue')
+
+# thechnical analysis
+require(TTR)
+getSymbols("AAPL")
+chartSeries(AAPL)
+addMACD()
+addBBands()
+
