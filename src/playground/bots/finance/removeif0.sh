a04b30f121f02060b69164db8911103183c0e191
dangermouse <dangermaus@users.sf.net>
2013-11-11 16:57:49 +0100

    removing finance bot as it is superseeded by R

--- src/playground/bots/finance/removeif0.sh
+++
@@ -1,7 +0,0 @@
-if [ -f "$1" ] 
-then
-	FILESIZE=$(du -k "$1" | awk '{print $1}')
-	if [ "$FILESIZE" -eq "0" ]; then
-  	   rm $1
-	fi
-fi
--- src/playground/bots/finance/sleeprandom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-r=$(( $RANDOM % 70 ));
-TIMEOUT=130
-let "TIMEOUT = $TIMEOUT + $r"
-echo "Random numer is $r"
-echo "Sleeping $TIMEOUT seconds..."
-sleep $TIMEOUT
