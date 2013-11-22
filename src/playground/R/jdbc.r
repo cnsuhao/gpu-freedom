0a1b9677d5da36b444ccbb9beb935650d336e2ff
dangermouse <dangermaus@users.sf.net>
2013-11-22 10:33:31 +0100

    automating calculation of total VaR

--- src/playground/R/jdbc.r
+++ src/playground/R/jdbc.r
@@ -6,10 +6,16 @@ drv <- JDBC("com.mysql.jdbc.Driver", paste(drive,"\\jdbc\\mysql-connector-java-5
 b_conn  <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/bitcoin", username, password);
 b_pricetable <- dbReadTable(b_conn, "pricevalue")
 b_price      <- dbGetQuery(b_conn, "select price from pricevalue order by id asc")
-b_last_price  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt>(NOW() - INTERVAL 7 DAY)  order by id asc")
+b_last_prices  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt>(NOW() - INTERVAL 7 DAY)  order by id asc")
+b_last_price  <- dbGetQuery(b_conn, "select price from pricevalue where create_dt=(select max(create_dt) from pricevalue)")
 b_avg_price   <- dbGetQuery(b_conn, "SELECT DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG( price ) as usd  FROM pricevalue GROUP BY date ORDER BY date")
 
 b_avg_price_last_60 <- dbGetQuery(b_conn, "SELECT DATE_FORMAT( create_dt,  '%Y-%m-%d' ) AS date, AVG( price ) as usd  FROM pricevalue GROUP BY date ORDER BY date LIMIT 60")
+
+b_btc_shortterm_open <- dbGetQuery(b_conn, "select btc from wallet where name='shortterm' and id=(select max(id) from wallet where name='shortterm');")
+b_btc_midterm_open <- dbGetQuery(b_conn, "select btc from wallet where name='midterm' and id=(select max(id) from wallet where name='midterm');")
+b_btc_longterm_open <- dbGetQuery(b_conn, "select btc from wallet where name='longterm' and id=(select max(id) from wallet where name='longterm');")
+b_btc_tiz_open <- dbGetQuery(b_conn, "select btc from wallet where name='tiz' and id=(select max(id) from wallet where name='tiz');")
 
 p_conn      <- dbConnect(drv, "jdbc:mysql://127.0.0.1:3306/powergrid", username, password);
 p_freqtable <- dbReadTable(p_conn, "tbfrequency")
--- src/playground/R/performanceanalytics.r
+++ src/playground/R/performanceanalytics.r
@@ -10,12 +10,6 @@ percentdiff <- function(x) {
   return(xdiff)
 }
 
-# compute value at risk for bitcoins
-computeVarBTC <- function(btc_open, btc_price) {
-  #requires jdbc.r to be loaded
-  pct <- percentdiff(b_avg_price_last_60$usd)
-  return(VaR(pct, p = 0.95)[1]*btc_open*btc_price)
-}  
 
 # general compute value at risk
 computeVar <- function(x, openpos, price) {
@@ -23,12 +17,41 @@ computeVar <- function(x, openpos, price) {
   return(VaR(pct, p = 0.95)[1]*openpos*price)
 }
 
+
 # source("C:\\xampp\\htdocs\\gpu_freedom\\src\\playground\\R\\jdbc.r")
 # source("C:\\xampp\\htdocs\\gpu_freedom\\src\\playground\\R\\performanceanalytics.r")
-# I have open 0.3 BTC and the current price is 500$
-# computeVarBTC(0.3,500)
+# computeVarBTC(0.3) # i currently own 0.3 BTC 
 # it returns the amount of money i can loose on the next day 
 # with 95% confidence level and 60 days time horizon in the past
+
+# compute value at risk for bitcoins
+#requires jdbc.r to be resourced
+computeVarBTC <- function(btc_open) {
+  print("last price (USD):")
+  print(b_last_price$price)
+  print("Value at Risk, 60 day interval, 95% confidence level (USD):")
+  return(computeVar(b_avg_price_last_60$usd, btc_open, b_last_price$price))
+}  
+
+computeTotalVarBTC <- function() {
+
+ print("short term open (BTC)")
+ print(b_btc_shortterm_open$btc) 
+ print("mid term open (BTC)")
+ print(b_btc_midterm_open$btc)
+ print("long term open (BTC)")
+ print(b_btc_longterm_open$btc) 
+ print("tiz open (BTC)")
+ print(b_btc_tiz_open$btc)
+ 
+ total = b_btc_shortterm_open$btc + b_btc_midterm_open$btc + b_btc_longterm_open$btc + b_btc_tiz_open$btc
+ print ("total open (BTC)")
+ print(total)
+
+ computeVarBTC(total)
+}
+
+
 
 
 
