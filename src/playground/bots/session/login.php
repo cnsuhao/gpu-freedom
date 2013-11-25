e279053673c643750442aaa32d36770392abe6bc
dangermaus <dangermaus@users.sourceforge.net>
2013-11-26 00:16:26 +0100

    added login form

---
+++ src/playground/bots/session/login.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form method="post" action="login_2.php">
+<b>Username:</b><br>
+<input type="text" name="name" />
+<br>
+<b>Password:</b><br>
+<input type="text" name="pwd" />
+<br>
+<input type="submit" value="login" />
+</form>
+</body>
+</html>
---
+++ src/playground/bots/session/login_2.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<html>
+<body>
+<?php
+include("config.inc.php");
+$name=mysql_real_escape_string($_POST['name']);
+$pwd=mysql_real_escape_string($_POST['pwd']);
+If ($name=="") exit;
+
+if (($name==$username) && ($pwd=$password)) {
+    echo "<b>login ok :-)</b>";
+	$_SESSION['userbot'] = $name;
+} else {
+    $_SESSION['userbot'] = "";
+	die("<b>login failed</b>");
+}
+
+?>
+</body>
+</html>
