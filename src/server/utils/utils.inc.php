cbeaff532c90dc113e2357ba4fb7c4aa35d5e692
dangermaus <dangermaus@users.sourceforge.net>
2016-03-09 21:51:40 +0100

    added getPHPVersion function

--- src/server/utils/utils.inc.php
+++ src/server/utils/utils.inc.php
@@ -28,4 +28,12 @@ function js_cookie($name,$value)
         print "</script>";
 }
 
+function getPHPVersion() {
+	  if (!defined('PHP_VERSION_ID')) {
+		$version = explode('.', PHP_VERSION);
+		define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+	  }
+	  return PHP_VERSION_ID;
+}
+
 ?>
