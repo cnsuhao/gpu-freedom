edf0dcc342a55ac38b528d8ada545a106683681d
dangermouse <dangermaus@users.sf.net>
2016-07-14 14:26:12 +0200

    added index page

---
+++ src/playground/bots/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>GPU bots</title>
+</head>
+<body>
+<h2>GPU bots</h2>
+
+<ul>
+<li><a href="powergrid/index.php">Powergrid</a></li>
+<li><a href="meteo/index.php">Meteo</a></li>
+<li><a href="runofriver/index.php">Run Of River</a></li>
+</ul>
+
+</body>
+</html>
