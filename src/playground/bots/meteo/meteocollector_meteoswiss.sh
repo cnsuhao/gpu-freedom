428a4f585a071875c03cc59dc7d9a0824af27024
dangermouse <dangermaus@sourceforge.net>
2016-07-15 23:14:53 +0200

    added working directory to shell scripts

--- src/playground/bots/meteo/meteocollector_meteoswiss.sh
+++ src/playground/bots/meteo/meteocollector_meteoswiss.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # main page is ?
+cd "$(dirname "$0")"
 rm -rf data.csv
 wget -O data.csv --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" "http://data.geo.admin.ch/ch.meteoschweiz.swissmetnet/VQHA69.csv"
 ./removeif0.sh data.csv
--- src/playground/bots/runofriver/runofriver_collector.sh
+++ src/playground/bots/runofriver/runofriver_collector.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # main page is ?
+cd "$(dirname "$0")"
+
 rm -rf data.xml
 wget -O data.xml --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" "http://www.hydrodaten.admin.ch/lhg/SMS.xml"
 ./removeif0.sh data.xml
