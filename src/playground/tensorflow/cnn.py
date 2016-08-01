7920bcee734a6ac6734a8c3aa84421fcc6b4deb6
dangermouse <dangermaus@sourceforge.net>
2016-08-01 15:15:37 +0200

    doc

--- src/playground/tensorflow/cnn.py
+++ src/playground/tensorflow/cnn.py
@@ -59,7 +59,7 @@ accuracy=tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 
-for i in range(1000):
+for i in range(500):
     batch = mnist.train.next_batch(50)
     if i%100==0:
         train_accuracy=sess.run(accuracy, feed_dict={x:batch[0],y_:batch[1],
@@ -71,30 +71,3 @@ for i in range(1000):
 print("test accuracy %g"% sess.run(accuracy, feed_dict={x: mnist.test.images,
                                                         y_:  mnist.test.labels,
                                                         keep_prob: 1.0}) )
-
-
-
-
-'''
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-
-print(x.get_shape())
-
-y = tf.nn.softmax(tf.matmul(x,W)+b)
-
-
-cross_entropy = - tf.reduce_sum(y_*tf.log(y))
-
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
-
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    correct_prediction=tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
-    accuracy=tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-'''
