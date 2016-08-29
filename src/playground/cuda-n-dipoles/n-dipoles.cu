5d867f70b020ff9be100d2d9531f34a61ea193b0
dm <dm@deepmind.home>
2016-08-30 00:02:01 +0200

    ongoing effort

--- src/playground/cuda-n-dipoles/n-dipoles.cu
+++ src/playground/cuda-n-dipoles/n-dipoles.cu
@@ -4,9 +4,10 @@
 // to execute the example ./n-dipoles.out
 #include <stdio.h>
 #define N 640 // number of dipoles
+//TODO: adjust these constants
 #define t 0.0001 // timestamp
 #define r 10e-15 // rutheford radius
-
+#define q 1e-10 //electric quant
 
 __global__ void simulate_dipoles(double *x, double *y, double *omega, double *E_pot) {
 	double ax[N*2]; double ay[N*2]; 
@@ -15,6 +16,28 @@ __global__ void simulate_dipoles(double *x, double *y, double *omega, double *E_
 	//if (tid<N)
 	//	c[tid] = a[tid] + b[tid];
 }
+
+/*
+__device__ double getElectricForce(int p, int q) {
+	/*	
+	double E_force=0;	
+	if ((p<N) && (q<N)) {
+		E_force=1;
+	}
+	return E_force;
+	*/
+//	return 0;
+//}
+
+/*
+__device__ double getDistance(int p, int q, double *x, double *y) {
+	double distance = 0;
+	if ((p<N) && (q<N)) {
+		distance = sqrt((x[p]-x[q])*(x[p]-x[q])+(y[p]-y[q])*(y[p]-y[q]));		
+	}
+	return distance;
+}
+*/
 
 int main(void) {
 	double x[N*2]; double y[N*2]; 
@@ -29,6 +52,7 @@ int main(void) {
         cudaMalloc( (void**)&dev_omega, N*sizeof(int));
         cudaMalloc( (void**)&dev_E_pot, N*sizeof(int));
 
+	//TODO: init variables with Box-Muller
 	for (int i=0; i<N*2; i++) {
 		x[i] = -i,
 		y[i] = i*i;
