58ac752aeff1061f45099c04722b4822e3ac97c7
dm <dm@deepmind.home>
2016-09-03 10:26:29 +0200

    several bugfixes

--- src/playground/cuda-n-dipoles/n-dipoles.cu
+++ src/playground/cuda-n-dipoles/n-dipoles.cu
@@ -3,7 +3,8 @@
 // to make it: nvcc n-dipoles.cu -o n-dipoles.out
 // to execute the example ./n-dipoles.out
 #include <stdio.h>
-#define N 640 // number of dipoles
+#define Np 1280 // number of particles (dipoles are half of them), should be CUDA core count
+#define Nd Np/2 // number of dipoles
 //TODO: adjust these constants
 #define T 0.0001 // timestamp
 #define R 10e-15 // rutheford radius
@@ -19,7 +20,7 @@ __device__ double sqr(double x) {
 
 __device__ double getDistanceSquared(int p1, int p2, double *x, double *y) {
 	double distance = 0;
-	if ((p1<N) && (p2<N)) {
+	if ((p1<Np) && (p2<Np)) {
 		distance = sqr(x[p1]-x[p2])+sqr((y[p1]-y[p2]));		
 	}
 	return distance;
@@ -55,7 +56,7 @@ __device__ void getElectricAcceleration(int p1, int p2,
 	else    mass = Me; // odd indexes are electrons
 		
 
-	if ((p1<N) && (p2<N)) {
+	if ((p1<Np) && (p2<Np)) {
 		acceleration = COULOMB * Q * Q * getDistanceSquared(p1, p2, x, y) / mass;  
 		// now we need to project acceleration along (x1-x2) and (y1-y2)
 		projectVector(acceleration, x[p1], x[p2], y[p1], y[p2], ax, ay);
@@ -64,18 +65,18 @@ __device__ void getElectricAcceleration(int p1, int p2,
 }
 
 
-__global__ void simulate_dipoles(double *x, double *y, double *omega, double *E_pot) {
-	double ax[N*2]; double ay[N*2]; double angle[N*2];
-        	
+__global__ void simulate_dipoles(double *x, double *y, double *omega, 
+				 double *ax, double *ay, double *angle,
+                                 double *E_pot) {
 	int tid = blockIdx.x; 
         int iselectron;
 	double ax_temp; double ay_temp;
 
-	   if (tid<N) {
+	   if (tid<Np) {
 		// 1. calculate acceleration on tid particle
 		ax[tid]=0; ay[tid]=0;
 
-		for (int i=0; i<N; i++) {
+		for (int i=0; i<Np; i++) {
 			if (i==tid) continue; // we do not calculate on ourself
 			iselectron = (i % 2);
 			// we do not calculate acceleration on the partner particle on the dipole
@@ -93,43 +94,63 @@ __global__ void simulate_dipoles(double *x, double *y, double *omega, double *E_
 
 
 int main(void) {
-	double x[N*2]; double y[N*2]; 
-	double omega[N];
-        double E_pot[N]; // potential energy of the system
-
+	double x[Np]; double y[Np]; double omega[Nd];
+        double ax[Np]; double ay[Np]; double angle[Nd];
+        double E_pot[Nd]; // potential energy of the system
+        
         double *dev_x, *dev_y, *dev_omega;
+	double *dev_ax, *dev_ay, *dev_angle;
 	double *dev_E_pot;
 
-	cudaMalloc( (void**)&dev_x,     N*2*sizeof(double));
-        cudaMalloc( (void**)&dev_y,     N*2*sizeof(double));
-        cudaMalloc( (void**)&dev_omega, N*sizeof(int));
-        cudaMalloc( (void**)&dev_E_pot, N*sizeof(int));
+	cudaMalloc( (void**)&dev_x,     Np*sizeof(double));
+        cudaMalloc( (void**)&dev_y,     Np*sizeof(double));
+        cudaMalloc( (void**)&dev_omega, Nd*sizeof(int));
+	cudaMalloc( (void**)&dev_ax,    Np*sizeof(double));
+	cudaMalloc( (void**)&dev_ay,    Np*sizeof(double));
+	cudaMalloc( (void**)&dev_angle, Nd*sizeof(double));
+        cudaMalloc( (void**)&dev_E_pot, Nd*sizeof(int));
 
 	//TODO: init variables with Box-Muller
-	for (int i=0; i<N*2; i++) {
+	for (int i=0; i<Np; i++) {
 		x[i] = -i,
 		y[i] = i*i;
+		ax[i] = 0;
+		ay[i] = 0;
+	}
+	for (int i=0; i<Nd; i++) {
 		omega[i] = 0;
+		angle[i] = 0;
 		E_pot[i] = 0;
 	}
 
-        cudaMemcpy(dev_x, x, N*2*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_y, y, N*2*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_omega, omega, N*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_E_pot, E_pot, N*sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_x, x, Np*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_y, y, Np*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_omega, omega, Nd*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_ax, x, Np*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_ay, x, Np*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_angle, x, Nd*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_E_pot, E_pot, Nd*sizeof(double), cudaMemcpyHostToDevice);
 	
-	simulate_dipoles<<<N,1>>>(dev_x, dev_y, dev_omega, dev_E_pot);
-
-	cudaMemcpy(omega, dev_omega, N*sizeof(double), cudaMemcpyDeviceToHost);
-	cudaMemcpy(E_pot, dev_E_pot, N*sizeof(double), cudaMemcpyDeviceToHost);
+	simulate_dipoles<<<Np,1>>>(dev_x, dev_y, dev_omega, dev_ax, dev_ay, dev_angle, dev_E_pot);
 	
-	for (int i=0; i<N; i++) {
-		printf("i: %d omega: %g E_pot %g\n", i, omega[i], E_pot[i]);
+	cudaMemcpy(x, dev_x, Np*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(y, dev_y, Np*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(omega, dev_omega, Nd*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(ax, dev_ax, Np*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(ay, dev_ay, Np*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(angle, dev_angle, Nd*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(E_pot, dev_E_pot, Nd*sizeof(double), cudaMemcpyDeviceToHost);
+	
+	for (int i=0; i<Np; i++) {
+		printf("i: %d ax: %g ay %g\n", i, ax[i], ay[i]);
 	}
 	
 	cudaFree(dev_x);
         cudaFree(dev_y);
 	cudaFree(dev_omega);
+	cudaFree(dev_ax);
+	cudaFree(dev_ay);
+	cudaFree(dev_angle);
 	cudaFree(dev_E_pot);
 
 }
