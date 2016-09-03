2d60aa2e436637774ac5bb1cb190f01d599fbde0
dm <dm@deepmind.home>
2016-09-03 09:51:34 +0200

    calculating coulomb acceleration2

--- src/playground/cuda-n-dipoles/n-dipoles.cu
+++ src/playground/cuda-n-dipoles/n-dipoles.cu
@@ -5,39 +5,92 @@
 #include <stdio.h>
 #define N 640 // number of dipoles
 //TODO: adjust these constants
-#define t 0.0001 // timestamp
-#define r 10e-15 // rutheford radius
-#define q 1e-10 //electric quant
+#define T 0.0001 // timestamp
+#define R 10e-15 // rutheford radius
+#define Q 1 //electric quant
+#define PI 3.1415926535
+#define COULOMB 1/(4*PI)
+#define Mp 1  // proton mass
+#define Me 1  // electron mass
+
+__device__ double sqr(double x) {
+	return x*x;
+}
+
+__device__ double getDistanceSquared(int p1, int p2, double *x, double *y) {
+	double distance = 0;
+	if ((p1<N) && (p2<N)) {
+		distance = sqr(x[p1]-x[p2])+sqr((y[p1]-y[p2]));		
+	}
+	return distance;
+}
+
+
+// Projects a magnitude along x and y axis
+__device__ void projectVector(double magnitude,
+                              double x1, double x2, double y1, double y2, 
+                              double *px, double* py) {
+
+	double distance;
+	double deltax; double deltay;
+	
+	deltax = (x2-x1);
+	deltay = (y2-y1);
+	distance = sqrt(sqr(deltax) + sqr(deltay));	
+
+	// TODO: double check if projection formula is correct
+	*px = magnitude * deltax / distance; 
+	*py = magnitude * deltay / distance;
+	
+}  
+
+__device__ void getElectricAcceleration(int p1, int p2, 
+                                        double *x, double *y, double *ax, double *ay) {
+	
+	// acceleration is calculated on p1	
+	double acceleration=0;
+	double mass;
+	if ((p1 % 2) == 0) // even indexes are proton
+		mass = Mp;
+	else    mass = Me; // odd indexes are electrons
+		
+
+	if ((p1<N) && (p2<N)) {
+		acceleration = COULOMB * Q * Q * getDistanceSquared(p1, p2, x, y) / mass;  
+		// now we need to project acceleration along (x1-x2) and (y1-y2)
+		projectVector(acceleration, x[p1], x[p2], y[p1], y[p2], ax, ay);
+	}
+
+}
+
 
 __global__ void simulate_dipoles(double *x, double *y, double *omega, double *E_pot) {
 	double ax[N*2]; double ay[N*2]; 
         	
 	int tid = blockIdx.x; 
-	//if (tid<N)
-	//	c[tid] = a[tid] + b[tid];
+        int iselectron;
+	double ax_temp; double ay_temp;
+
+	   if (tid<N) {
+		// 1. calculate acceleration on tid particle
+		ax[tid]=0; ay[tid]=0;
+
+		for (int i=0; i<N; i++) {
+			if (i==tid) continue; // we do not calculate on ourself
+			iselectron = (i % 2);
+			// we do not calculate acceleration on the partner particle on the dipole
+			if ((iselectron==1) && (i==tid-1)) continue;
+			if ((iselectron==0) && (i==tid+1)) continue;	
+			
+			getElectricAcceleration(tid, i, x, y, &ax_temp, &ay_temp);
+			ax[tid]=ax[tid]+ax_temp;
+			ay[tid]=ay[tid]+ay_temp;
+		}
+	   }
 }
 
-/*
-__device__ double getElectricForce(int p, int q) {
-	/*	
-	double E_force=0;	
-	if ((p<N) && (q<N)) {
-		E_force=1;
-	}
-	return E_force;
-	*/
-//	return 0;
-//}
 
-/*
-__device__ double getDistance(int p, int q, double *x, double *y) {
-	double distance = 0;
-	if ((p<N) && (q<N)) {
-		distance = sqrt((x[p]-x[q])*(x[p]-x[q])+(y[p]-y[q])*(y[p]-y[q]));		
-	}
-	return distance;
-}
-*/
+
 
 int main(void) {
 	double x[N*2]; double y[N*2]; 
