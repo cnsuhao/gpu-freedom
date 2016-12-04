c442b91c6bbebed96e1b9bdba1c049b977fcacd8
dangermaus <dangermaus@users.sourceforge.net>
2016-12-04 15:47:30 +0100

    simple backup mechanism

---
+++ src/playground/bots/epicshelter/backup_bots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+sudo rm -f dump_bots.sql
+sudo rm -f dump_bots.tar 
+sudo rm -f dump_bots.tar.gz
+
+sudo mysqldump -u mybots -pbots13? --databases meteo news powergrid raspi runofriver > dump_bots.sql
+sudo tar -cf dump_bots.tar dump_bots.sql
+sudo gzip dump_bots.tar
+scp dump_bots.tar.gz pi@firstlight:backup/
