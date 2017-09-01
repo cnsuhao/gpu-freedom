ac9691ca11c71df2cf4898fd65b3f9b424d67e4d
dangermaus <dangermaus@users.sourceforge.net>
2017-09-01 20:47:36 +0200

    initial release

---
+++ src/playground/bots/gridcoin/polorexcexbot/conf/example_config.inc.php
@@ -0,0 +1,18 @@
+<?php
+$poloniex_api_key="";
+$poloniex_api_secret="";
+$poloniex_deposit_1=""; // address to deposit currency 1
+$poloniex_deposit_2=""; // address to deposit currency 2
+
+$bittrex_api_key="";
+$bittrex_api_secret="";
+$bittrex_deposit_1=""; 
+$bittrex_deposit_2="";
+
+$ccex_api_key="";
+$ccex_api_secret="";
+//$ccex_deposit_1="";  //TODO:implement me
+//$ccex_deposit_2="";
+
+
+?>
