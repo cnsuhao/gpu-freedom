98d9d24dad75e68e114ef6b0fe0ef88f1e4a78da
dangermaus <dangermaus@users.sourceforge.net>
2017-09-11 23:57:56 +0200

    rotating logs

---
+++ src/playground/bots/gridcoin/polorexcexbot/rotate_eth.sh
@@ -0,0 +1,17 @@
+rm polorexcex_eth.log.14
+mv polorexcex_eth.log.13 polorexcex_eth.log.14
+mv polorexcex_eth.log.12 polorexcex_eth.log.13
+mv polorexcex_eth.log.11 polorexcex_eth.log.12
+mv polorexcex_eth.log.10 polorexcex_eth.log.11
+mv polorexcex_eth.log.9 polorexcex_eth.log.10
+mv polorexcex_eth.log.8 polorexcex_eth.log.9
+mv polorexcex_eth.log.7 polorexcex_eth.log.8
+mv polorexcex_eth.log.6 polorexcex_eth.log.7
+mv polorexcex_eth.log.5 polorexcex_eth.log.6
+mv polorexcex_eth.log.4 polorexcex_eth.log.5
+mv polorexcex_eth.log.3 polorexcex_eth.log.4
+mv polorexcex_eth.log.2 polorexcex_eth.log.3
+mv polorexcex_eth.log.1 polorexcex_eth.log.2
+mv polorexcex_eth.log polorexcex_eth.log.1
+touch polorexcex_eth.log
+
