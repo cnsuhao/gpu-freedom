ddd1dec6f75ddd8af313570d5aaaf59d52b7e4c1
dangermaus <dangermaus@users.sourceforge.net>
2017-09-11 22:21:19 +0200

    added an ethereum arbitrage bot

---
+++ src/playground/bots/gridcoin/polorexcexbot/polorexcexrunner_eth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# main page is ?
+cd "$(dirname "$0")"
+
+wget -O store_polorexcex_eth_log --no-proxy http://127.0.0.1/gpu_freedom/src/playground/bots/gridcoin/polorexcexbot/polorexcex_eth.php 
+
+cat store_polorexcex_eth_log >> polorexcex_eth.log
+cat store_polorexcex_eth_log
+
+
