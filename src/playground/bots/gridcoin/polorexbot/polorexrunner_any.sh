10b3ad04e823fcc75e3b7ae45ca64f6e5b17818e
dangermaus <dangermaus@users.sourceforge.net>
2017-09-14 17:57:26 +0200

    from grc to any

--- src/playground/bots/gridcoin/polorexbot/polorexrunner_any.sh
+++ src/playground/bots/gridcoin/polorexbot/polorexrunner_any.sh
@@ -2,7 +2,7 @@
 # main page is ?
 cd "$(dirname "$0")"
 
-wget -O store_polorex_log --no-proxy http://127.0.0.1/gpu_freedom/src/playground/bots/gridcoin/polorexbot/polorex_grc.php 
+wget -O store_polorex_log --no-proxy http://127.0.0.1/gpu_freedom/src/playground/bots/gridcoin/polorexbot/polorex_any.php 
 
 cat store_polorex_log >> polorex.log
 cat store_polorex_log
