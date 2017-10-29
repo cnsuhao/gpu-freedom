01078224250a0030f8e77375ab3e64b30dd15e71
dangermaus <dangermaus@users.sourceforge.net>
2017-10-29 14:06:19 +0100

    url changed

--- src/playground/bots/meteo/meteocollector_meteoswiss.sh
+++ src/playground/bots/meteo/meteocollector_meteoswiss.sh
@@ -2,7 +2,7 @@
 # main page is ?
 cd "$(dirname "$0")"
 rm -rf data.csv
-wget -O data.csv --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" "http://data.geo.admin.ch/ch.meteoschweiz.swissmetnet/VQHA69.csv"
+wget -O data.csv --no-check-certificate -U "Mozilla/5.0 (Windows NT 6.1; rv:2.0b7pre) Gecko/20100921 Firefox/4.0b7pre" "https://data.geo.admin.ch/ch.meteoschweiz.swissmetnet/VQHA69.csv"
 ./removeif0.sh data.csv
 if [ -f data.csv ] 
 then
